735a95905f69e06574a046e0bd09244c9d27918a
kigogie1 Talbot <kigogie22@gmail.com>
2018-11-26 10:22:53 -0500

    StarWars first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'start-wars-assesment'`, () => {
+  it(`should have as title 'star-wars-assesment'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('start-wars-assesment');
+    expect(app.title).toEqual('star-wars-assesment');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to start-wars-assesment!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to star-wars-assesment!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'start-wars-assesment';
+  title = 'star-wars-assesment';
 }
